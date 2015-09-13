5837545dc2e5109c00c510eb846097fdff66f3ef
jays <jaymichael78@hotmail.com>
2015-09-13 14:30:42 -0500

    Sending my work

--- codeFiles/plot3.R
+++ codeFiles/plot3.R
@@ -18,7 +18,7 @@ setwd(copyDir)
 plot(feb2007$Time,feb2007$Sub_metering_1,ylab="Energy sub metering",type="l",xlab="")
 plot.xy(xy.coords(feb2007$Time,feb2007$Sub_metering_2),type="l",col="red")
 plot.xy(xy.coords(feb2007$Time,feb2007$Sub_metering_3),type="l",col="blue")
-legend("topright", legend = c("Sub_metering_1", "Sub_metering_2","Sub_metering_3"),lty=c(1,1,1),col=c("black","red","blue"))
+legend("topright", legend = c("Sub_metering_1", "Sub_metering_2","Sub_metering_3"),lty=c(1,1,1),col=c("black","red","blue"), xjust=0, cex=0.75)
 
 #Make png file
 dev.copy(png, file="plot3.png", width=480, height=480)
--- codeFiles/plot4.R
+++ codeFiles/plot4.R
@@ -21,7 +21,7 @@ plot(feb2007$Time,feb2007$Voltage,ylab="Voltage",type="l",xlab="datetime")
 plot(feb2007$Time,feb2007$Sub_metering_1,ylab="Energy sub metering",type="l",xlab="")
 plot.xy(xy.coords(feb2007$Time,feb2007$Sub_metering_2),type="l",col="red")
 plot.xy(xy.coords(feb2007$Time,feb2007$Sub_metering_3),type="l",col="blue")
-legend("topright", legend = c("Sub_metering_1", "Sub_metering_2","Sub_metering_3"),lty=c(1,1,1),col=c("black","red","blue"))
+legend("topright", legend = c("Sub_metering_1", "Sub_metering_2","Sub_metering_3"),lty=c(1,1,1),col=c("black","red","blue"), bty="n", xjust=0, cex=0.5)
 plot(feb2007$Time,feb2007$Global_reactive_power,ylab="Global_reactive_power",type="l",xlab="datetime")
 
 #Make png file
